d2e1dbec0d5a7cd9bd42be34b8f98031676aa271
reggi <thomas@reggi.com>
2014-03-24 02:19:02 -0400

    build script changes

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-wintersmith build && git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
+wintersmith build
+#git push origin `git subtree split --prefix build gh-pages`:gh-pages --force
+git subtree split --prefix build --branch gh-pages
+git push origin gh-pages
